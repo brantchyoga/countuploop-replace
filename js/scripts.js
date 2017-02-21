d49e6e8970c9ff62ca39d91588613825ce1c69cb
brant porter <brantrporter@gmail.com>
2017-02-21 10:58:47 -0800

    function to count up and replace vowels with -

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+$(function(){
+  $("form#numberPlay").submit(function(event) {
+  event.preventDefault();
+  var number = parseFloat($("#number").val());
+  var endNumber = parseFloat($("#endNumber").val());
+  var total = []
+
+    if (number>endNumber){
+      alert("Count to number has to be greater than number!");
+    } else {
+        for(var i=number; i<=endNumber; i+=number){
+          total.push(i);
+        };
+      };
+  $("#output").text(total);
+  });
+});
+$(function(){
+  $("form#wordPlay").submit(function(event) {
+    event.preventDefault();
+    var sentence = $("#sentence").val();
+    console.log(sentence);
+    var vowels = ["a", "o", "i", "u", "e"]
+    sentence = sentence.replace(/[aeuoi]/gi, "-");
+    for(var i=0;i<=sentence.length; i++){
+      sentence = sentence.replace("a", "-")
+      sentence = sentence.replace("e", "-")
+      sentence = sentence.replace("i", "-")
+      sentence = sentence.replace("o", "-")
+      sentence = sentence.replace("u", "-")
+      console.log(sentence);
+    };
+    $("#output2").text(sentence);
+    $("#sentence").hide();
+
+  });
+});
